006451d3d0f01d16d0bb9d86f1ad6e974f2f1414
Lucas Morales <lucas@lucasem.com>
2016-05-06 03:32:52 -0400

    added Luby MIS

---
+++ scheme/algs/luby_mis.scm
@@ -0,0 +1,47 @@
+;;; LUBY'S MAXIMAL INDEPENDENT SET
+
+(define-record-type state
+  (fields (immutable num-nodes)
+          (mutable r)
+          (mutable active)
+          (mutable status)
+          (mutable round)))
+
+(define (state-init uid num-nodes start-node)
+  (make-state num-nodes 0 #t '() 0))
+
+(define (transition state edges-in edges-out gui)
+  (state-round-set! state (+ (state-round state) 1))
+
+  (if (state-active state)
+    (cond
+      ;; ROUND 1
+      ((= 1 (mod (state-round state) 2))
+       (if (> (length (edges->msgs edges-in)) 0)
+           (state-status-set! state 'loser))
+       (if (not (null? (state-status state)))
+           (state-active-set! state #f))
+       (state-r-set! state (random-integer (expt (state-num-nodes state) 5)))
+       (msg-spam! edges-out (state-r state)))
+      ;; ROUND 2
+      (else
+       (let ((max-r (apply max (edges->msgs edges-in))))
+         (if (or (null? max-r)
+                 (> (state-r state) max-r))
+             (state-status-set! state 'winner)))
+       (if (eqv? 'winner (state-status state))
+           (begin (msg-spam! edges-out 'winner)
+                  (gui-edge-highlight-all gui edges-out "green")
+                  (gui-edge-text-all gui edges-out))))))
+
+  ;; GUI
+  (let ((status (state-status state)))
+    (if (not (state-active state))
+        (gui-node-highlight gui (if (eqv? status 'winner)
+                                    "green"
+                                    "lightgray"))))
+  )
+
+(define luby-mis (make-algorithm state-init transition))
+(set-algorithm! luby-mis)
+
